fec619a499d823e78a0ecd307d4380ab9d352d6b
skybber <lada.dvorak7@gmail.com>
2015-10-04 18:12:19 +0200

    Technology Cocktail example added.

---
+++ ejb-cdi-hibernate-jsf/src/main/java/org/hotswap/agent/example/em/EntityManagerProducer.java
@@ -0,0 +1,25 @@
+package org.hotswap.agent.example.em;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class EntityManagerProducer {
+
+    @Inject
+    EntityManagerFactory emf;
+
+    @Produces
+    @ConversationScoped
+    public EntityManager create() {
+        return emf.createEntityManager();
+    }
+
+    public void destroy(@Disposes EntityManager em) {
+        em.close();
+    }
+
+}
