6b880e68a63fabcc2a9f6fc1b2b96da530c78905
skybber <lada.dvorak7@gmail.com>
2018-02-17 19:04:30 +0100

    Add JSF ViewScoped bean test

---
+++ deltaspike-hibernate-jsf/src/main/java/org/hotswap/agent/example/deltaspike/beans/JsfViewBean.java
@@ -0,0 +1,36 @@
+package org.hotswap.agent.example.deltaspike.beans;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class JsfViewBean implements Serializable {
+
+    private static final long serialVersionUID = -8176648612947203701L;
+
+    private static int idCount = 0;
+
+    private int beanId;
+
+    @Inject
+    private HelloProducer1 helloProducer1;
+
+    @Inject
+    private HelloProducer2 helloProducer2;
+
+    private int helloCount = 0;
+
+    public JsfViewBean() {
+        beanId = idCount;
+        idCount ++;
+    }
+
+    public String hello() {
+        helloCount ++;
+        return "JsfViewBean[beanId=" + beanId + ",helloCount=" + helloCount + "]:" + helloProducer2.hello();
+    }
+}
