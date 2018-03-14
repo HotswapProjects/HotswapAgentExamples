9f6432771f71ae60813725249208db376b50a522
skybber <lada.dvorak7@gmail.com>
2018-03-14 21:00:08 +0100

    Watch support

--- deltaspike-hibernate-jsf/buildAndRunDebug.sh
+++ deltaspike-hibernate-jsf/buildAndRunDebug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t deltaspike-hibernate-jsf .
-docker run -ti --rm -p 8080:8080 -p 8000:8000 -v `pwd`/target/classes:/extra_class_path -v `pwd`/src/main/webapp:/webapp_dir deltaspike-hibernate-jsf
+docker run -ti --rm -p 8080:8080 -p 8000:8000 -v `pwd`/target/classes:/extra_class_path -v `pwd`/src/main/webapp:/webapp_dir -v /var/run/docker.sock:/var/run/docker.sock deltaspike-hibernate-jsf
