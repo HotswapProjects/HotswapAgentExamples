d4f4dcd4af03a6d365731f76614a17899beb172a
Jiří Bubník <bubnik@datalite.cz>
2018-05-04 16:04:50 +0200

    New Spring boot plugin test

---
+++ spring-boot/src/main/java/org/hotswap/agent/example/springboot/Test.java
@@ -0,0 +1,5 @@
+package org.hotswap.agent.example.springboot;
+
+public class Test {
+    public int version() { return 0; }
+}
---
+++ spring-boot/src/main/java/org/hotswap/agent/example/springboot/Test2.java
@@ -0,0 +1,9 @@
+package org.hotswap.agent.example.springboot;
+
+public class Test2 {
+    public int version() { return 1; }
+
+    public void test() {
+
+    }
+}
