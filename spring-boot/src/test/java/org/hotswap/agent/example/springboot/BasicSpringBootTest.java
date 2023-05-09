ab67e7014fdc939733788e5562bb173ffa2a05df
Ian Luo <ian.luo@gmail.com>
2023-05-09 10:55:59 +0800

    make spring-boot a module
    Signed-off-by: Ian Luo <ian.luo@gmail.com>

--- spring-boot/src/test/java/org/hotswap/agent/example/springboot/BasicSpringBootTest.java
+++ spring-boot/src/test/java/org/hotswap/agent/example/springboot/BasicSpringBootTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BasicSpringBootTests {
+public class BasicSpringBootTest {
 	@Autowired
 	ApplicationContext applicationContext;
 
